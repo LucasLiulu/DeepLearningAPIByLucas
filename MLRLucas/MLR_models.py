e80c1faa85b5cb191472e133e1deba66c4359f4f
lu.liu <lu.liu@inveno.com>
2019-01-29 09:46:51 +0800

    添加早停功能。

--- MLRLucas/MLR_models.py
+++ MLRLucas/MLR_models.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from MLR_core import MLRCore
 from tqdm import tqdm
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, log_loss
 import numpy as np
 import tensorflow as tf
 import numbers, sklearn, os
@@ -32,7 +32,9 @@ def batcher(X_, y_=None, w_=None, batch_size=-1):
             ret_w = w_[i: upper_bound]
         yield (ret_x, ret_y, ret_w)
 
-def batch_feeddict(X, y, w, core):
+def batch_feeddict(X, y=None, w=None, core=None):
+    if core is None:
+        raise ValueError('must give a core!')
     fd = {}
     fd[core.X_train] = X.astype(np.float32)
     if y is not None:
@@ -43,7 +45,7 @@ def batch_feeddict(X, y, w, core):
 
 class MLRClassifier(MLRCore):
     def __init__(self, seed=None, session_config=None, n_epoches=100,
-                 batch_size=-1, verbose=0,
+                 batch_size=-1, verbose_eval=0,
                  **core_arguments):
         core_arguments['seed'] = seed
         self.seed = seed
@@ -52,9 +54,22 @@ class MLRClassifier(MLRCore):
         self.n_epoches = n_epoches
         self.batch_size = batch_size
         self.steps = 0
-        self.verbose = verbose
+        self.verbose_eval = verbose_eval
+        self.early_stopping_rounds = 0    # 记录是否达到早停次数
 
-    def fit(self, X, y, sample_weight=None, pos_class_weight=None, n_epochs=None, show_progress=False):
+    def fit(self, X, y, sample_weight=None, pos_class_weight=None, n_epochs=None, metric=None,
+            show_progress=False, maximize=None, verbose_eval=None, early_stopping_rounds=None):
+        '''
+
+        :param X:
+        :param y:
+        :param sample_weight: balanced or None
+        :param pos_class_weight: array like or None
+        :param n_epochs:
+        :param show_progress:
+        :param early_stopping_rounds:
+        :return:
+        '''
         if not (set(y) == set([0, 1])):
             raise ValueError('Input labels must be in set {0, 1}.')
 
@@ -64,7 +79,8 @@ class MLRClassifier(MLRCore):
         if pos_class_weight:
             self.pos_class_weight = pos_class_weight
         used_w = self._preprocess_sample_weights(sample_weight, pos_class_weight, y)
-        self._fit(X, y, used_w, n_epochs, show_progress)
+        self._fit(X, y, used_w, n_epochs, show_progress, maximize=maximize, metric=metric,
+                  verbose_eval=verbose_eval, early_stopping_rounds=early_stopping_rounds)
 
     '''
     根据正负样本比例调整样本权重
@@ -96,10 +112,25 @@ class MLRClassifier(MLRCore):
         self.session = tf.Session(config=self.session_config, graph=self.core.graph)
         self.session.run(self.core.init_all_vars)
 
-    def _fit(self, X_, y_, w_, n_epochs=None, show_progress=False):
+    def _fit(self, X_, y_, w_, n_epochs=None, show_progress=False, maximize=False,
+             verbose_eval=None, early_stopping_rounds=None, metric='logloss'):
+        '''
+        训练模型的核心类。
+        其中包括模型的训练流程和早停。
+        :param X_: 输入训练数据，需要是pandas DataFrame格式
+        :param y_: 训练数据对应的标签，同样需要是pandas Dataframe格式
+        :param w_: 不平衡样本权重，是numpy 数组，数组元素是float类型
+        :param n_epochs:训练轮数，int类型
+        :param show_progress:是否显示进度条，bool类型
+        :param maximize:对于默写评价指标，是变小好还是变大好，bool类型。如果是变大好，则为TRUE；否则为FALSE
+        :param verbose_eval:每训练多少轮打印一次评价指标的值。int类型
+        :param early_stopping_rounds:这么多轮训练模型的指标都没有变好，则停止训练
+        :param metric:训练过程中使用的评价指标，可用于早停
+        :return:
+        '''
         if self.core.n_features is None:
             self.core.set_num_features(X_.shape[1])
-        assert  self.core.n_features == X_.shape[1], 'Different num of features in initialized graph and input'
+        assert self.core.n_features == X_.shape[1], 'Different num of features in initialized graph and input'
 
         if self.core.graph is None:
             self.core.build_graph()
@@ -111,24 +142,49 @@ class MLRClassifier(MLRCore):
         if self.seed:
             np.random.seed(self.seed)
 
+        if verbose_eval:
+            self.verbose_eval = verbose_eval
+
+        # 添加早停
+        if early_stopping_rounds:
+            earlyStopClass = EarlyStopClass(maximize_score=maximize,
+                                            stopping_rounds=early_stopping_rounds,
+                                            verbose=bool(verbose_eval),
+                                            metric=metric)
+        break_epoch = False    # 用于早停
+        metric_dict = {'auc': roc_auc_score, 'logloss': log_loss}    # 评价函数
         for epoch in tqdm(range(n_epochs), unit='epoch', disable=(not show_progress)):
+            if break_epoch:
+                break
             # 返回一个洗牌后的矩阵副本
-            # print('X_ shape: ', X_.shape)
             perm = np.random.permutation(X_.shape[0])
             epoch_loss = []
             for bX, by, bw in batcher(X_.iloc[perm], y_.iloc[perm], w_[perm], batch_size=self.batch_size):
-                fd = batch_feeddict(bX, by, bw, core=self.core)
+                fd = batch_feeddict(bX, y=by, w=bw, core=self.core)
                 ops_to_run = [self.core.trainer_op, self.core.loss, self.core.outputs]
                 _, batch_loss, predict = self.session.run(ops_to_run, feed_dict=fd)
                 epoch_loss.append(batch_loss)
                 self.steps += 1
 
-            if self.verbose:
-                print('[epoch {}]: mean loss value: {}'.format(epoch, np.mean(epoch_loss)))
+            # 训练时的模型评价指标
+            valided_fd = batch_feeddict(X_, y=y_, w=w_, core=self.core)
+            batch_loss_, predict_ = self.session.run([self.core.loss, self.core.outputs], feed_dict=valided_fd)
+            if metric == 'logloss':
+                metric_value = np.mean(batch_loss)
+            else:
+                metric_value = metric_dict[metric](y_, predict_)
+            if self.verbose_eval:
+                if epoch % self.verbose_eval == 0:
+                    print('[epoch {}]: metric {} value: {}'.format(epoch, metric, metric_value))
 
-            # print('predict shape: ', predict.shape, 'predict: ', predict)
-            # print("y_ iloc perm shape: ", y_.iloc[perm])
-            # print('auc: ', roc_auc_score(y_.iloc[perm], predict))
+            if early_stopping_rounds:
+                evaluation_result = metric_value
+                if earlyStopClass.callback(epoch, evaluation_result):
+                    print('\nearly_stopping_rounds: ', early_stopping_rounds)
+                    self.best_score = earlyStopClass.best_score
+                    self.best_iteration = earlyStopClass.best_iteration
+                    self.best_msg = earlyStopClass.best_msg
+                    break_epoch = True
 
     def decision_function(self, X, pred_batch_size=None):
         if self.core.graph is None:
@@ -171,4 +227,38 @@ class MLRClassifier(MLRCore):
     def destory(self):
         self.session.close()
         self.core.graph = None
+
+
+class EarlyStopClass(object):
+    def __init__(self, stopping_rounds=None, verbose=None, metric=None, maximize_score=None):
+        self.best_msg = ''
+        self.best_score = float(np.inf)
+        self.best_iteration = float(np.inf)
+        self.stopping_rounds = stopping_rounds
+        self.verbose = verbose    # True or False
+        self.maximize_score = maximize_score     # 用于指明指标优化方向，是往小了优化还是往大了优化，往大优化为TRUE
+
+        msg = ("Multiple eval metrics have been passed: "
+               "'{0}' will be used for early stopping.\n\n")
+        print(msg.format(metric))
+        msg = "Will train until {} hasn't improved in {} rounds.\n"
+        print(msg.format(metric, stopping_rounds))
+
+
+    def callback(self, iteration=0, evaluation_result=0):
+        best_score = self.best_score * (-1) if (self.best_score is np.inf and self.maximize_score) else self.best_score
+        best_iteration = self.best_iteration
+        if (self.maximize_score and evaluation_result > best_score) or (not self.maximize_score and evaluation_result < best_score):
+            msg = '[%d]\t%s' % (iteration, '\t'.join(evaluation_result)) if isinstance(evaluation_result, list) \
+                else '[%d]\t%s' % (iteration, '\t' + str(evaluation_result))
+            self.best_msg = msg
+            self.best_score = evaluation_result
+            self.best_iteration = iteration
+            return False
+        elif iteration - best_iteration >= self.stopping_rounds:
+            best_msg = self.best_msg
+            if self.verbose:
+                msg = "Stopping. Best iteration: \n{}\n\n"
+                print(msg.format(best_msg))
+            return True
 
--- MLRLucas/MLR_test.py
+++ MLRLucas/MLR_test.py
@@ -3,7 +3,7 @@
 from MLR import MLRClass, get_data
 from MLR_models import MLRClassifier
 from tqdm import tqdm
-from sklearn.metrics import roc_auc_score, accuracy_score
+from sklearn.metrics import roc_auc_score, accuracy_score, log_loss
 
 # for m in tqdm([5, 10, 15, 20, 1], unit='m', disable=False):
 #     MLRClass().train(m=m, n_epoch=200)
@@ -11,9 +11,16 @@ from sklearn.metrics import roc_auc_score, accuracy_score
 train_x, train_y, test_x, test_y = get_data()
 data_length, n_features = train_x.shape
 mlr = MLRClassifier(m=5, seed=12, n_features=n_features, data_length=data_length, batch_size=100)
-mlr.fit(train_x, train_y, show_progress=True)
+mlr.fit(train_x, train_y, show_progress=False, early_stopping_rounds=10, n_epochs=10000, verbose_eval=1, metric='logloss', maximize=False)
+print('mlr.best_score: ', mlr.best_score)
+print("mlr.best_iteration: ", mlr.best_iteration)
+print("mlr.best_score: ", mlr.best_score)
+
 pred = mlr.predict(test_x)
 prob = mlr.predict_proba(test_x)
+print(pred)
+print(prob)
 print('accuracy: ', accuracy_score(test_y, pred))
+print('log_loss: ', log_loss(test_y, prob[:, 1]))
 print('auc: ', roc_auc_score(test_y, prob[:, 1]))
 mlr.save_state('model', 'model.ckpt')
